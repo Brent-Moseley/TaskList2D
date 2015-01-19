1d0373cf020d309a9104c024a04299ba8d7815a8
Brent Moseley <bmoseley@revspringinc.com>
2015-01-19 11:06:09 -0700

    Basic Mocha / Chai set up

--- test/test.js
+++ test/test.js
@@ -1,4 +1,10 @@
-var assert = require("assert")
+var assert = require("assert");
+var expect = require('chai').expect;
+var assert = require('chai').assert;
+var Item = require('../app/models/item');
+
+//  Just a basic test to make sure mocha is set up correctly
+//  To run from the test directory: mocha 
 describe('Array', function(){
   describe('#indexOf()', function(){
     it('should return -1 when the value is not present', function(){
@@ -7,3 +13,24 @@ describe('Array', function(){
     })
   })
 })
+
+// May need Chai for this:
+describe('User', function(){
+  var user;
+  beforeEach(function () {
+      user = new Item();
+  });  
+	it('should have a User object', function(){
+	  assert.typeOf(user, 'Object', 'user is an Item');
+	})
+  it('should have a User object with a name property', function(){
+    expect(user).to.have.property('name');
+  })  
+  afterEach(function () {
+    // No cleanup stuff to do here yet.
+  });  
+})
+
+
+//   http://stackoverflow.com/questions/19298118/what-is-the-role-of-describe-in-mocha
+//   http://chaijs.com/guide/styles/#expect
